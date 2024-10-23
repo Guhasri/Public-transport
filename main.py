8a9564b42611eb718093343e79c082dabd8a4f3a
Guhasri <guhasri2005@gmail.com>
2024-10-23 21:57:37 +0530

    run modifications

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 
-# Step 1: Preprocess the data
 os.system('python src/data_preprocessing.py')
 
-# Step 2: Train the models
+os.system('python src/feature_engineering.py')
+
 os.system('python src/model_training.py')
 
-# Step 3: Make predictions
-os.system('python src/make_predictions.py')
+os.system('streamlit run bus_demand_app.py')
